552687787583d5721b74640862a29425cbd22551
JeonJaeeun <100048395+JeonJaeeun@users.noreply.github.com>
2024-03-24 11:05:48 +0900

    [Silver IV] Title: 행운의 바퀴, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/2840. 행운의 바퀴/행운의 바퀴.cc
+++ 백준/Silver/2840. 행운의 바퀴/행운의 바퀴.cc
@@ -1,67 +1,24 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-typedef pair<int, char> ci;
-
-//특정 화살표에서 시작한 행운의 바퀴 리턴
-string arrowStartWheel(int arrow_point, int n, vector<char>& wheel) {
-	string ans = "";
-	int start = arrow_point;
-	do {
-		ans += wheel[arrow_point];
-		arrow_point = (arrow_point + 1) % n;
-	} while (arrow_point != start);
-	return ans;
-}
-
-//행운의 바퀴 구하는 함수
-string solution(int n, vector<ci>& record) {
-	vector<char> wheel(n, '?'); //바퀴의 상태
-	vector<bool> check(26, false); //알파벳 중복 체크
-
-	int idx = 0; //화살표가 가리키는 인덱스
-	for (int i = 0; i < record.size(); i++) {
-		int rot = record[i].first;
-		char alpha = record[i].second;
-		idx = (idx - rot % n + n) % n;
-		if (wheel[idx] == alpha) {
-			continue;
+#include<cstdio>
+#include<string.h>
+int n,m,p,a,v[26];
+char d[27],b;
+int main(){
+	scanf("%d %d",&n,&m);
+	memset(d,'?',sizeof(char)*n);
+	while(m--){
+		scanf("%d %c",&a,&b);
+		a%=n;
+		p-=a;
+		if(p<0)p=n+p;
+		if((d[p]!='?'&&d[p]!=b)||(v[b-'A']&&d[p]!=b)){
+			printf("!");
+			return 0;
 		}
-		if (wheel[idx] != '?' || check[alpha - 'A']) {
-			return "!";
-		}
-		wheel[idx] = alpha;
-		check[alpha - 'A'] = true;
+		d[p]=b;
+		v[b-'A']=1;
 	}
-	return arrowStartWheel(idx, n, wheel);
-}
-
-/*
-	[행운의 바퀴]
-
-	바퀴가 돌아갈 필요는 없다.
-	화살표가 가르키는 인덱스를 회전 정보에 따라 바꿔준다.
-
-	1. 화살표가 가르키는 칸이 결정되지 않았으면, 알파벳을 바퀴에 적는다.
-	2. 이미 알파벳이 써 있는 경우, 같은 알파벳이 아닌 경우, 조건에 해당하는 바퀴를 만들 수 없다.
-	3. 바퀴에 쓰여있는 알파벳은 중복되지 않으므로, 동일한 알파벳이 여러 자리에 있을 수 없다.
-
-	n : 바퀴의 칸 수
-	k : 바퀴를 돌리는 횟수
-*/
-
-int main() {
-	int n, k;
-
-	//입력
-	cin >> n >> k;
-	vector<ci> record(k, { 0,0 }); //바퀴 회전 기록
-	for (int i = 0; i < k; i++) {
-		cin >> record[i].first >> record[i].second;
+	for(m=0;m<n;m++){
+		if(p+m>=n)p=-m;
+		printf("%c",d[p+m]);
 	}
-
-	//연산 & 출력
-	cout << solution(n, record);
-	return 0;
 }
