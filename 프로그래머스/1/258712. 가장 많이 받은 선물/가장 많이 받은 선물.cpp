84a9d291cbf096334145183c26f96f9e939e6b1b
JeonJaeeun <100048395+JeonJaeeun@users.noreply.github.com>
2024-09-08 00:37:30 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 7.93 ms, Memory: 4.55 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
@@ -1,73 +1,11 @@
-#include <string>
-#include <vector>
-#include <sstream>
-#include <map>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-using namespace std;
-
-int solution(vector<string> friends, vector<string> gifts) {
-    
-    int n = friends.size();
-    
-    /* 친구 이름을 인덱스로 변환: map 사용 */
-    map<string, int> gift_dict;
-    for(int i = 0; i < n; i++){
-        gift_dict[friends[i]] = i;
-    }
-    
-    /* 정의 */
-    // 서로 주고받은 선물 : 2차원 배열
-    vector<vector<int>> gift_table(n, vector<int>(n,0));
-    
-    // 선물 지수
-    vector<int> gift_score(n,0);
-    
-    // 총 선물 수
-    vector<int> total_gift(n,0);
-    
-    /* gifts 배열 -> a,b로 변환 */
-    for(string gift: gifts){
-        string a, b;
-        
-        stringstream ss(gift);
-        ss >> a >> b;
-        
-        /* a -> b 준 선물 기록 -> a 선물지수 증가, b 선물지수 감소 */
-        gift_table[gift_dict[a]][gift_dict[b]]++;
-        gift_score[gift_dict[a]]++;
-        gift_score[gift_dict[b]]--;
-    }
-    
-    /* 다음 달 선물 계산 */
-    for(int i = 0; i < n; ++i){
-        for(int j = 0; j < n; ++j){
-            // i = j 비교하지 않음
-            if(i == j){
-                continue;
-            }
-            
-            // i가 j보다 선물을 더 많이 줬으면, 다음달 i += 1
-            if(gift_table[i][j] > gift_table[j][i]){
-                total_gift[i] += 1;
-            }
-            
-            // i와 j의 준 선물 개수가 같으면, 선물 지수 비교
-            // 작은 선물 지수를 가진 사람이 줘야함 (큰 선물 지수를 가진 사람 다음달 += 1)
-            else if(gift_table[i][j] == gift_table[j][i]){
-                if(gift_score[i] > gift_score[j]){
-                    total_gift[i] += 1;
-                }
-            }
-        }
-    }
-    
-    /* 가장 선물 많이 받는 사람 */
-    int max_gift = total_gift[0];
-    for(int i = 0; i < total_gift.size(); ++i){
-        if(total_gift[i] > max_gift){
-            max_gift = total_gift[i];
-        }
-    }
-    return max_gift;
-
+// friends_len은 배열 friends의 길이입니다.
+// gifts_len은 배열 gifts의 길이입니다.
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int solution(const char* friends[], size_t friends_len, const char* gifts[], size_t gifts_len) {
+    int answer = 0;
+    return answer;
 }
